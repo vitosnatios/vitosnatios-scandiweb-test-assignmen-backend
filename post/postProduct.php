aedbb20a03087eedb0f7dd293f811a01fd113938
unknown <vitosdeveloper@gmail.com>
2023-03-23 06:36:27 -0300

    some major api changes

--- post/postProduct.php
+++ post/postProduct.php
@@ -3,11 +3,8 @@ include 'Create.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $body = json_decode(file_get_contents('php://input'));
-
 
 if (
   $_SERVER['REQUEST_METHOD'] === 'POST'
--- post/removeProductById.php
+++ post/removeProductById.php
@@ -1,15 +1,14 @@
 <?php
 include 'Delete.php';
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+
 
 if (
-  $_SERVER['REQUEST_METHOD'] === 'DELETE'
+  $_SERVER['REQUEST_METHOD'] === 'POST'
 ) {
-  // parse_str(file_get_contents('php://input'), $_DELETE);
+  parse_str(file_get_contents('php://input'), $_DELETE);
   $_DELETE = json_encode(urldecode(file_get_contents('php://input')));
   if (file_get_contents('php://input') && $_DELETE) {
     $replaced = str_replace(["id=", '"', "[", "]"], "", $_DELETE);
